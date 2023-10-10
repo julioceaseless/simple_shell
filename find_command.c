5ddcf1fa527fd53eaf42d0ee47918dcf2b428c68
CHIGODI <chiegody254@gmail.com>
2023-10-10 15:07:24 +0300

    Add find_command.c

---
+++ find_command.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <sys/stat.h>
+
+/**
+ * find_command - finds command in paths
+ * @paths: array of full command paths to search in
+ *
+ * Return: path to where command was found or NULL if not found
+ */
+char *find_command(char **paths)
+{
+	struct stat st;
+	int i = 0, found;
+
+	if (paths != NULL)
+	{
+		while (paths[i] != NULL)
+		{
+			found = stat(paths[i], &st);
+			if (found == 0)
+			{
+				return (paths[i]);
+			}
+			i++;
+		}
+	}
+	return (NULL);
+}
