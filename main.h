beb20581fd7bfc569081393aaf9fb7de6b7921f3
julioceaseless <julioceaseless@gmail.com>
2023-10-14 07:17:38 +0000

    update to main header

--- main.h
+++ main.h
@@ -7,7 +7,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-extern char **environ;
 #define BUF_SIZE 120
 /**
  * struct built_in - function pointer struct
@@ -17,9 +16,10 @@ extern char **environ;
 struct built_in
 {
 	char *command;
-	void (*func)(void);
+	void (*func)(char *);
 };
 typedef struct built_in built_t;
+extern char **environ;
 /* prototypes */
 void execute(char *args, char *argv);
 char **token(char *command, char *delimeter);
@@ -27,9 +27,12 @@ char *find_command(char **paths);
 char *_getenv(const char *name);
 int check_path(const char *command);
 char **append_path(char *path, char *command);
-void print_env(void);
-void my_exit(void);
-void (*handle_built_in(char *key))(void);
 char *stringfy(char *argv[], int argc);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+char *_strtok(char *str, const char *delim);
+/* Shell built in  */
+void (*handle_built_in(char *key))(char *);
+void print_env(char *args);
+void my_exit(char *args);
+void change_dir(char *args);
 #endif
