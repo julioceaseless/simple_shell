91baf694bd2b07ddcd837a7f1e7a13e66401c479
CHIGODI <chiegody254@gmail.com>
2023-10-16 12:40:32 +0300

    Implement shell built in cd Add change_dir file

--- change_dir.c
+++ change_dir.c
@@ -1,5 +1,25 @@
 #include "main.h"
 /**
+ * all_spaces - checks if a string contains only spaces
+ * @str: string to be checked
+ *
+ * Return: 1 if true 0 if false
+ *
+ */
+int all_spaces(char *str)
+{
+	while (*str)
+	{
+		if (!isspace((unsigned char)*str))
+		{
+			return (0);
+		}
+		str++;
+	}
+	return (1);
+}
+
+/**
  * change_dir - changes directory
  * @args: arguments with directory to move to
  *
@@ -9,26 +29,39 @@
 void change_dir(char *args)
 {
 	char **cmd = NULL;
-	char delim[] = " ";
-	char *home = _getenv("HOME");
+	char *delim = " ", *curr = _getenv("PWD=");
+	char *home = _getenv("HOME="),  *old = _getenv("OLDPWD=");
 	int ch_dir;
 
 	cmd = token(args, delim);
-	if (cmd != NULL && cmd[1] != NULL)
+	if (cmd != NULL && cmd[1] != NULL && !all_spaces(cmd[1]))
 	{
-		ch_dir = chdir(cmd[1]);
-		if (ch_dir != 0)
+		if (*cmd[1] == '-' && (strlen(cmd[1]) == 1))
 		{
-			perror("chdir");
-			return;
+			ch_dir = chdir(old);
+			if (ch_dir != 0)
+			{
+				perror("chdir");
+				return;
+			}
+			setenv("OLDPWD", curr, 1);
+			setenv("PWD", old, 1);
+			printf("%s\n", curr);
+		}
+		else
+		{
+			ch_dir = chdir(cmd[1]);
+			if (ch_dir != 0)
+			{
+				perror("cd");
+				return;
+			}
 		}
 	}
 	else
 	{
 		if (home != NULL)
-		{
 			chdir(home);
-		}
 		else
 		{
 			perror("chdir");
