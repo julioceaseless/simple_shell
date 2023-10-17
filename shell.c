3b16aa56b0e8069605fe0103db511cf9f35f6b47
julioceaseless <julioceaseless@gmail.com>
2023-10-17 22:57:09 +0000

    betty

--- shell.c
+++ shell.c
@@ -1,18 +1,6 @@
 #include "shell.h"
 #include <signal.h>
 /**
- * signal_handle - checks for signal
- * @signal: value of the signal sent
- */
-void signal_handle(int signal)
-{
-	if (signal == SIGINT)
-	{
-		write(STDOUT_FILENO, "\n$ ", 3);
-	}
-}
-
-/**
  * is_whitespace - checks if a string consists of only whitespace characters
  * @str: string to check
  * Return: 1 if the string contains only whitespace, 0 otherwise
@@ -34,6 +22,67 @@ int is_whitespace(const char *str)
 	return (1);
 }
 /**
+ * read_handler - handles read output
+ * @read: value of read system call
+ * @line: line read from stdin
+ * @argv: name of the program
+ */
+void read_handler(ssize_t read, char *line, char *argv[])
+{
+	void (*function)(char *) = NULL;
+
+	if (read == -1)
+	{
+		free(line);
+		perror("_getline");
+		exit(1);
+	}
+	if (read == 0)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		free(line);
+		exit(0);
+	}
+	if (is_whitespace(line))
+	{
+		free(line);
+		return;
+	}
+	if (line[read - 1] == '\n')
+	{
+		if (line[0] == '\n')
+		{
+			free(line);
+			return;
+		}
+		line[read - 1] = '\0';
+	}
+
+	function = handle_built_in(line);
+	if (function == NULL)
+	{
+		execute(line, argv[0]);
+	}
+	else
+	{
+		function(line);
+	}
+	free(line);
+}
+
+/**
+ * signal_handle - checks for signal
+ * @signal: value of the signal sent
+ */
+void signal_handle(int signal)
+{
+	if (signal == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n$ ", 3);
+	}
+}
+
+/**
  * main - Entry point
  * @argc: argument count
  * @argv: arguments
@@ -44,43 +93,22 @@ int main(int argc, char *argv[])
 	size_t len = 0;
 	ssize_t read;
 	char *line = NULL, *str = NULL;
-	void (*function)(char *) = NULL;
 
 	if (argc > 1)
 	{
-		str = remove_space_padding(stringfy(argv, argc));
+		str = stringfy(argv, argc);
 		execute(str, argv[0]);
 		free(str);
 	}
+
 	signal(SIGINT, signal_handle);
+
 	while (1)
 	{
 		write(STDOUT_FILENO, "$ ", 2);
 		read = _getline(&line, &len, stdin);
-		if (read == -1)
-		{
-			free(line);
-			perror("_getline");
-			break;
-		}
-		if (read == 0)
-		{
-			write(STDOUT_FILENO, "\n", 1);
-			free(line);
-			break;
-		}
-		if (is_whitespace(line))
-			continue;
-		if (read > 0 && line[read - 1] == '\n')
-		{
-			if (line[0] == '\n')
-				continue;
-			line[read - 1] = '\0';
-			read--;
-		}
-		function = handle_built_in(line);
-		(function == NULL) ? execute(line, argv[0]) : function(line);
+		read_handler(read, line, argv);
 	}
-	free(line);
 	return (0);
 }
+
