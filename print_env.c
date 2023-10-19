c185c8980d18ad63a98bfe5657c2f2bb98b4e115
julioceaseless <julioceaseless@gmail.com>
2023-10-19 12:40:09 +0000

    print environment variables

--- print_env.c
+++ print_env.c
@@ -1,19 +1,24 @@
 #include "shell.h"
 /**
  * print_env - Display Enviroment Variable
- * @cmd: command line args
- * @errnum: errnum count
+ * @cmd: command line arg but not utilized
+ * @errnum: errnum count but not utilized
  * Return: Always 0
  */
-int print_env(__attribute__((unused)) char **cmd,
-		__attribute__((unused)) int errnum)
+int print_env(char **cmd, int errnum)
 {
-	size_t i;
+	char **env = environ;
+	(void)cmd;
+	(void)errnum;
 
-	for (i = 0; environ[i] != NULL; i++)
+	if (env == NULL)
+		return (0);
+
+	while (*env != NULL)
 	{
-		write(1, environ[i], strlen(environ[i]));
+		write(STDOUT_FILENO, *env, strlen(*env));
 		write(STDOUT_FILENO, "\n", 1);
+		env++;
 	}
 	return (0);
 }
