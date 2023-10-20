0fbb9530502c0d1b7b0e8fc7cd63668f0497bfb4
julioceaseless <julioceaseless@gmail.com>
2023-10-20 09:19:24 +0000

    betty style corrections

--- my_exit.c
+++ my_exit.c
@@ -9,40 +9,39 @@
  */
 void my_exit(char **cmd_list, char *inpt_read, char *shell_name, int errnum)
 {
-    int i = 0;
-    int exit_status
+	int i = 0;
+	int exit_status;
 
-    if (cmd_list[1] == NULL)
-    {
-        free(inpt_read);
-        free(cmd_list);
-        exit(EXIT_SUCCESS);
-    }
+	if (cmd_list[1] == NULL)
+	{
+		free(inpt_read);
+		free(cmd_list);
+		exit(EXIT_SUCCESS);
+	}
 
-    while (cmd_list[1][i] != '\0')
-    {
-        if (isalpha(cmd_list[1][i]) != 0)
-        {
-            custm_perror(shell_name, errnum, cmd_list);
-            break;
-        }
-        else
-        {
-	     exit_status = atoi(cmd_list[1]);
-
-            if (exit_status < 0 || errnum == ENOENT)
-            {
-                free(inpt_read);
-                free(cmd_list);
-                _exit(2);
-            }
-            else
-            {
-                free(inpt_read);
-                free(cmd_list);
-                _exit(exit_status & 0xFF);
-            }
-        }
-        i++;
-    }
+	while (cmd_list[1][i] != '\0')
+	{
+		if (isalpha(cmd_list[1][i]) != 0)
+		{
+			custm_perror(shell_name, errnum, cmd_list);
+			break;
+		}
+		else
+		{
+			exit_status = atoi(cmd_list[1]);
+			if (exit_status < 0 || errnum == ENOENT)
+			{
+				free(inpt_read);
+				free(cmd_list);
+				_exit(2);
+			}
+			else
+			{
+				free(inpt_read);
+				free(cmd_list);
+				_exit(exit_status & 0xFF);
+			}
+		}
+		i++;
+	}
 }
